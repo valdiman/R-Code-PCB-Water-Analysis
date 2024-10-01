17717655afbb06d85fc4e4e46bf568b82171b719
Andres Martinez <andres-martinez@uiowa.edu>
2024-10-01 11:10:17 -0500

    updated it

--- R/Global/DataMapping.R
+++ R/Global/DataMapping.R
@@ -39,7 +39,7 @@ install.packages("tidyr")
 }
 
 # Read data ---------------------------------------------------------------
-# Data in pg/L
+# Data (pg/L) downloaded from Pangaea using code: R/Pangaea/PangaeaDownloadDataset.R
 wdc <- read.csv("Data/USAWaterPCB.csv")
 
 # Extract sample site locations -------------------------------------------
