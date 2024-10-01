ce2dd08870d6d70f177fd52f2854ee31a0f5f157
Andres Martinez <andres-martinez@uiowa.edu>
2024-10-01 13:58:07 -0500

    deleted

--- app/global.R
+++
@@ -1,7 +0,0 @@
-# global.R
-library(shiny)
-library(leaflet)
-library(ggplot2) # Assuming you're using ggplot2 in your server logic
-
-# Load data
-wdc <- read.csv("WaterDataCongenerAroclor09072023.csv")
